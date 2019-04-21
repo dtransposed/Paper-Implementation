c896cbabdc8b9e01a316e6db4c89060f71c93457
Damian Bogunowicz <damian.bogunowicz@aid-driving.eu>
2019-04-21 12:31:32 +0200

    Finish DQN coding

--- test.py
+++
@@ -1,9 +0,0 @@
-import gym
-import cv2
-env = gym.make('PongDeterministic-v4')
-action = 0 # modify this!
-o = env.reset()
-for i in range(20): # repeat one action for five times
-    o = env.step(action)[0]
-    cv2.imshow('image', o)
-    cv2.waitKey(100)
